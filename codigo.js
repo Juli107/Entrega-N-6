a61a9a5d260f1068574ab2c4085d1908180e057a
Julián Cuello <juliancuello107@gmail.com>
2021-11-17 22:18:46 -0300

    Entrega N°6

---
+++ codigo.js
@@ -0,0 +1,16 @@
+//*ARRAYS
+
+let frutas = ["Peras", "Naranjas", "Bananas", "Manzanas"];
+
+let numeros = [(100, 20, 30, 5, 120, 200)];
+
+let mix = ["José", 70, "Pedro", "María", 50];
+
+console.log(frutas);
+console.log(numeros);
+console.log(mix);
+console.log(frutas.length);
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log("En la vuelta número " + i + " fruta: " + frutas[i]);
+}
